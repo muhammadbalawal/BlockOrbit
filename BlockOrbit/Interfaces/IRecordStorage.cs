afc9ec1431ea1b948dcf8e969e350d0003020c0f
balawal <balawal@DESKTOP-L42F2R8>
2025-02-27 19:42:02 -0800

    Add project files.

---
+++ BlockOrbit/Interfaces/IRecordStorage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockOrbit.Interfaces
+{
+    public interface IRecordStorage
+    {
+        /// <summary>
+        /// Effectively update an record
+        /// </summary>
+        void Update(uint recordId, byte[] data);
+
+        /// <summary>
+        /// Grab a record's data
+        /// </summary>
+        byte[] Find(uint recordId);
+
+        /// <summary>
+        /// This creates new empty record
+        /// </summary>
+        uint Create();
+
+        /// <summary>
+        /// This creates new record with given data and returns its ID
+        /// </summary>
+        uint Create(byte[] data);
+
+        /// <summary>
+        /// Similar to Create(byte[] data), but with dataGenerator which generates
+        /// data after a record is allocated
+        /// </summary>
+        uint Create(Func<uint, byte[]> dataGenerator);
+
+        /// <summary>
+        /// This deletes a record by its id
+        /// </summary>
+        void Delete(uint recordId);
+    }
+}
---
+++ BlockOrbit/Interfaces/ISerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockOrbit.Interfaces
+{
+    public interface ISerializer<K>
+    {
+        byte[] Serialize(K value);
+
+        K Deserialize(byte[] buffer, int offset, int length);
+
+        bool IsFixedSize
+        {
+            get;
+        }
+
+        int Length
+        {
+            get;
+        }
+    }
+}
